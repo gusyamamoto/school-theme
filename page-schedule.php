e91a20914d43de5f8ba4d586857be7b1d0be53a6
Gustavo <gustavoyamamoto@gmail.com>
2024-09-07 11:23:34 -0700

    Added ACF for schedule page

--- page-schedule.php
+++ page-schedule.php
@@ -21,7 +21,73 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php school_theme_post_thumbnail(); ?>
+
+	<div class="entry-content">
+    <?php the_content(); ?>
+
+	<?php if ( function_exists( 'get_field' ) ) {
+					if ( get_field( 'table_caption' ) ) {
+						the_field( 'table_caption' );
+					}
+				} ?>	
+
+    <?php if( have_rows('weekly_course_schedule') ): ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Course</th>
+                    <th>Instructor</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php while( have_rows('weekly_course_schedule') ) { the_row(); ?>
+                <tr>
+                    <td><?php the_sub_field('course_schedule_date'); ?></td>
+                    <td><?php the_sub_field('course_schedule_course'); ?></td>
+                    <td><?php the_sub_field('course_schedule_instructor'); ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+		<?php else: ?>
+			<p>No schedule available.</p>
+		<?php endif; ?>
+	</div>
+
+
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'school-theme' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID();
 
 
 		endwhile; // End of the loop.
@@ -30,5 +96,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
