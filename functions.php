714e07ecf3b68e494f36ddb73ac3c55647c11cde
Gustavo <gustavoyamamoto@gmail.com>
2024-09-07 22:53:44 -0700

    Removed Comments in the backend and added taxonomy template for students

--- functions.php
+++ functions.php
@@ -154,6 +154,21 @@ function school_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'school_theme_scripts' );
 
+
+// Hiding Comment in the backend
+
+function remove_comments_menu() {
+    remove_menu_page('edit-comments.php'); // Remove Comments menu
+}
+add_action('admin_menu', 'remove_comments_menu');
+
+//Change excerpt length to 20 words
+function fwd_excerpt_length( $length ) {
+	return 20;
+	}
+	add_filter( 'excerpt_length', 'fwd_excerpt_length', 999 );
+	
+
 /**
  * Implement the Custom Header feature.
  */
@@ -185,9 +200,3 @@ require get_template_directory() . '/inc/cpt-taxonomy.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-//Change excerpt length to 20 words
-function fwd_excerpt_length( $length ) {
-	return 20;
-	}
-	add_filter( 'excerpt_length', 'fwd_excerpt_length', 999 );
--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -86,9 +86,22 @@ function fwd_register_custom_post_types() {
         'capability_type'    => 'post',
         'has_archive'        => true,
         'hierarchical'       => false,
-        'menu_position'      => 10,
+        'menu_position'      => 6,
         'menu_icon'          => 'dashicons-welcome-learn-more',
-        'supports'           => array('title'),
+        'supports'           => array('title','editor'),
+        'template'           => array(
+            array('core/paragraph', array(
+                'placeholder' => 'Short Biography',
+            )),
+            array('core/button', array(
+                'placeholder' => 'Student Name Portfolio',
+                'url' => '#', 
+                'className' => 'portfolio-button'
+            )),
+    ),
+    
+    // Optional: Lock the template to prevent users from adding/removing blocks
+    'template_lock'      => 'all',
     );
     register_post_type('school-theme-student', $args);
 
@@ -124,5 +137,47 @@ function fwd_register_taxonomies() {
         'rewrite'           => array( 'slug' => 'staff-categories' ),
     );
     register_taxonomy( 'school-theme-staff-category', array( 'school-theme-staff' ), $args );
+
+    //Add Students Taxonomy
+    $labels = array(
+        'name'              => _x( 'Students Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Student Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Students Categories' ),
+        'all_items'         => __( 'All Students Category' ),
+        'parent_item'       => __( 'Parent Student Category' ),
+        'parent_item_colon' => __( 'Parent Student Category:' ),
+        'edit_item'         => __( 'Edit Student Category' ),
+        'view_item'         => __( 'View Student Category' ),
+        'update_item'       => __( 'Update Student Category' ),
+        'add_new_item'      => __( 'Add New Student Category' ),
+        'new_item_name'     => __( 'New Student Category Name' ),
+        'menu_name'         => __( 'Student Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'students-categories' ),
+    );
+    register_taxonomy( 'fwd_register_custom_post_types', array( 'school-theme-student' ), $args );
+
 }
 add_action( 'init', 'fwd_register_taxonomies');
+
+
+function school_change_title_text( $title ){
+    $screen = get_current_screen();
+  
+    if  ( 'school-theme-student' == $screen->post_type ) {
+         $title = 'Add student name';
+    }
+  
+    return $title;
+}
+  
+add_filter( 'enter_title_here', 'school_change_title_text' );
