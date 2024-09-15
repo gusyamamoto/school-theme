cc825003317014860d990600af756548fc78a40c
Gustavo <gustavoyamamoto@gmail.com>
2024-09-15 12:16:51 -0700

    Added styling to footer

--- footer.php
+++ footer.php
@@ -15,17 +15,23 @@
 		<nav id="footer-left" class="footer-left">
     		<?php wp_nav_menu( array( 'theme_location' => 'footer-left') ); ?>
 		</nav>
-
+		<div class="site-info">
+			<div>
+				<h2>Credits</h2>
+				<?php esc_html_e( 'Created by ', 'school-theme' ); ?><a href="<?php echo esc_url( __( 'https://wp.bcitwebdeveloper.ca/', 'school-theme' ) ); ?>"><?php esc_html_e( 'Beata Kozma & Jonathon Leathers.', 'school-theme' ); ?></a>
+			</div>
+			<div>
+				<?php esc_html_e( 'Photos courtesy of ', 'school-theme' ); ?><a href="<?php echo esc_url( __( 'https://www.shopify.com/stock-photos', 'school-theme' ) ); ?>"><?php esc_html_e( 'Burst.', 'school-theme' ); ?></a>
+			</div>
+			
+			
 		<nav id="footer-right" class="footer-right">
+			<h2>Links</h2>
     		<?php wp_nav_menu( array( 'theme_location' => 'footer-right') ); ?>
 		</nav>
 
 
 
-		<div class="site-info">
-		<?php esc_html_e( 'Created by ', 'school-theme' ); ?><a href="<?php echo esc_url( __( 'https://wp.bcitwebdeveloper.ca/', 'school-theme' ) ); ?>"><?php esc_html_e( 'Beata Kozma & Jonathon Leathers.', 'school-theme' ); ?></a>
-		<?php esc_html_e( 'Photos courtesy of ', 'school-theme' ); ?><a href="<?php echo esc_url( __( 'https://www.shopify.com/stock-photos', 'school-theme' ) ); ?>"><?php esc_html_e( 'Burst.', 'school-theme' ); ?></a>
-			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
